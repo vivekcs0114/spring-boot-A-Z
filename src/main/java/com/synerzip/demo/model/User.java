a449bdb6f19a67b88bdbdcbef8d0d3de92f361ed
Vivek Tiwari <vivekt@synerzipune.local>
2018-04-23 18:39:59 +0530

    swagger impl - contract bet developer and consumer.

--- src/main/java/com/synerzip/demo/model/User.java
+++ src/main/java/com/synerzip/demo/model/User.java
@@ -1,17 +1,23 @@
 package com.synerzip.demo.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel("All details about the user")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have atleast two character")
+    @ApiModelProperty("Name should have atleast two character")
     private String name;
 
     @Past
+    @ApiModelProperty("Date of Birth should be in past")
     private Date birthDate;
 
     public Integer getId() {
